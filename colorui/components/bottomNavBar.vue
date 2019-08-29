427944430add0fa36c7cd234b642335cdd064bfc
liangyingshao <1669807584@qq.com>
2019-08-29 17:10:04 +0800

    增加操作栏自定义组件

---
+++ colorui/components/bottomNavBar.vue
@@ -0,0 +1,33 @@
+<template>
+	<scroll-view class="nav text-center bg-white">
+		<view class="cu-item" :class="nav.id==TabCur?'text-blue cur':''" @tap="tabSelect" :data-id='nav.id' v-for="nav in navs" :key='nav.id'>
+			<view class="flex-sub">
+				<text :class="nav.icon"></text> {{nav.name}}
+			</view>
+		</view>
+	</scroll-view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				TabCur:0,
+				scrollLeft:0,
+			};
+		},
+		props:{
+			navs:Array,
+		},
+		methods:{
+			tabSelect(e) {
+				this.TabCur = e.currentTarget.dataset.id;
+				this.scrollLeft = (e.currentTarget.dataset.id - 1) * 60;
+				this.$emit('selectchange',e.currentTarget.dataset.id);
+			}
+		}
+	}	
+</script>
+
+<style>
+</style>
