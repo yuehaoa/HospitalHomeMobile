01902bb347daa780f0471a80da04e5051b741d9a
sh963852741 <sh963852741@126.com>
2019-08-31 12:11:09 +0800

    update

--- pages/Search/Result.vue
+++ pages/Search/Result.vue
@@ -4,7 +4,7 @@
 		<view class="cu-bar search bg-gray text-xxl" >
 			<span class="cuIcon-back_android margin-left" @tap="back()"></span>
 			<view class="search-form round text-xxl">
-				<input class="padding-left" :adjust-position="false" type="text" placeholder="原发性肝癌" confirm-type="search"></input>
+				<input class="padding-left" :adjust-position="false" type="text" v-model="keyword" confirm-type="search"></input>
 				<text class="cuIcon-voice text-blue" style="font-size: 44rpx;"></text>
 			</view>
 			<view class="action">
@@ -95,6 +95,7 @@
 				hidden: 'hidden',
 				Province:[],
 				index:0,
+				keyword:'原发性肝癌',
 				searchResult:[
 					{
 						name:'厦门大学附属翔安医院',
